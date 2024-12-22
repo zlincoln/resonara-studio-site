696c69ac6f75ec3ef331c1265b08540f8502ecce
Zachary Lincoln (personal) <zachlincoln@zlincoln-mac-mini.lan>
2024-12-21 21:32:38 -0500

    Cloudflare deploy fix?

--- astro.config.ts
+++ astro.config.ts
@@ -24,8 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'static',
-
+  output: 'server',
   integrations: [
     tailwind({
       applyBaseStyles: false,
